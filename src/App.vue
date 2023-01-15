83764e28ae479d900a7873ca88fdc8c28b2e3dd5
Allan <al.silvaquiroz@gmail.com>
2023-01-15 18:06:09 -0300

    docs: app.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import HelloWorld from "./components/HelloWorld.vue";
+// import TheWelcome from "./components/TheWelcome.vue";
+import WordlePage from './pages/WordlePage.vue';
+</script>
+
+<template>
+  <WordlePage/>
+</template>
+
+<style>
+
+*{
+  font-family: Roboto;
+}
+
+</style>
